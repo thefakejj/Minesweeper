37952dbc02c3112e510e4067c05b9226008f809a
thefakejj <jasper.laurila@gmail.com>
2023-04-14 19:56:16 +0300

    lisää yritystä

--- src/ui/game.py
+++ src/ui/game.py
@@ -11,6 +11,7 @@ class Minesweeper:
     def __init__(self):
         pygame.init()
         # if the size of the grid is selected in the menu, the caption could be pygame.display.set_caption(f"Minesweeper {scale}x{scale} or {horizontal}x{vertical}")
+        # the previous is impossible because the menu is drawn over an object of the Minesweeper class
         pygame.display.set_caption("Minesweeper")
 
         self.width = 640
@@ -18,7 +19,7 @@ class Minesweeper:
 
         self.surface = pygame.display.set_mode((self.width, self.height))
         
-        self.clock = pygame.time.Clock
+        self.clock = pygame.time.Clock()
 
         self.game_loop()
         self.menu()
@@ -36,12 +37,17 @@ class Minesweeper:
         while True:
             self.event_checker()
             pygame.display.flip()
+            self.clock.tick(60)
 
     def event_checker(self):
         for event in pygame.event.get():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 self.x_position = event.pos[0]
                 self.y_position = event.pos[1]
+            
+            if event.type == pygame.K_ESCAPE:
+                pygame.quit()
+                exit()
 
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -56,26 +62,18 @@ class Minesweeper:
     # def start_game(self, size):
     #     self.game_loop()
 
-    def set_minesweeper_size(value):
+    def set_minesweeper_size(self, value):
             if value == 1:
                 return 8
             if value == 2:
                 return 16
 
-    def menu():
-        
-
-
-
+    def menu(self):
         menu = pygame_menu.Menu('Settings', 400, 300,
                         theme=pygame_menu.themes.THEME_BLUE)
-        menu.add.button('Play', start_game(set_minesweeper_size))
+        menu.add.button('Play', start_game())
         menu.add.selector('Field size :', [('8x8', 1), ('16x16', 2)], onchange=set_minesweeper_size)
 
         menu.add.button('Quit', pygame_menu.events.EXIT)
-
-
-
-
 
         menu.mainloop(self.surface)
