a8e970d28a6c1fa21e897f309477d0a1bf8b0adc
thefakejj <jasper.laurila@gmail.com>
2023-05-11 15:08:04 +0300

    envin database path lisätty db_connectioniin

--- src/db_connection.py
+++ src/db_connection.py
@@ -1,11 +1,7 @@
-import os
 import sqlite3
+from config import DATABASE_FILE_PATH
 
-dirname = os.path.dirname(__file__)
-
-connection = sqlite3.connect(os.path.join(
-    dirname, "..", "data", "leaderboard.db"))
-
+connection = sqlite3.connect(DATABASE_FILE_PATH)
 
 def get_database_connection():
     return connection
